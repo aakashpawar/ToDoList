4c89236204375f625a8fd9714b52f5ff540ddb57
Aakash Pawar <aakash.pawar.cs@gmail.com>
2019-03-09 00:25:40 +0530

    deployment fixes

--- ToDo.Web/Program.cs
+++ ToDo.Web/Program.cs
@@ -21,6 +21,7 @@
         public static IWebHost CreateWebHostBuilder(string[] args)
         {
             return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
                 .UseStartup<Startup>()
                 .UseConfiguration(Configuration)
                 .Build();
